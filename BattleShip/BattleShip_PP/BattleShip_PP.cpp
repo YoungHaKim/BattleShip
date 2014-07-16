d0144027e1f797a4ce15af9e1cc04023cad847a2
vasthorizon <wizkid7@naver.com>
2014-07-16 17:37:21 +0900

    2주자 Pair Programming
    송지훈 : Navigator
    김영하 : Driver
    
    vector를 사용하여 ships를 관리
    
    destroy 시에 하나식 삭제하고
    다 destroy되면 게임이 끝나도록 구현했음

--- BattleShip/BattleShip_PP/BattleShip_PP.cpp
+++ BattleShip/BattleShip_PP/BattleShip_PP.cpp
@@ -2,11 +2,17 @@
 //
 
 #include "stdafx.h"
+#include <array>
+#include <vector>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	
 	std::string strInput, strResult;
+	srand((unsigned int)time(NULL));
+
+	std::vector<std::string> ships = { "Aircraft", "Battleship", "Cruiser", "Destroyer", "Destroyer" };
+	std::string results[] = { "HIT!", "MISS!", "DESTROY!" };
+
 	do
 	{
 		std::cout << "Input attack coordinates: type q to quit" << std::endl;
@@ -14,25 +20,32 @@ int _tmain(int argc, _TCHAR* argv[])
 
 		std::cout << "You typed " << strInput << std::endl;
 
-		srand(time(NULL));
-		int result = rand() % 3;
+		int resultIdx = rand() % _countof(results);
+		std::cout << results[resultIdx] << std::endl;
 
-		switch (result)
+		if (resultIdx == 2)
 		{
-		case 0: strResult = "HIT!"; break;
-		case 1: strResult = "MISS!"; break;
-		case 2: strResult = "DESTORY!"; break;
+			int destroyedShipIndex = rand() % ships.size();
+			printf_s("%s was destroyed\n", ships[destroyedShipIndex].c_str());
+			ships.erase(ships.begin() + destroyedShipIndex);
 
-		default:
-			strResult = "MISS";
-			break;
+			if (ships.size() == 0)
+			{
+				break;
+			}
+
+			printf("Ships: [");
+			for (int i = 0; i < ships.size(); i++)
+			{
+				printf("%s\t", ships[i].c_str());
+			}
+			printf("]\n\n");
+
 		}
-
-		std::cout << strResult << std::endl;
 
 	} while (strInput != "q");
 
-	std::cout << "Press ENTER to continue";
+	std::cout << "[GAME OVER!!!!!!!!] \n\n Press ENTER to continue";
 	fflush(stdin);
 	getchar();
 	return 0;
